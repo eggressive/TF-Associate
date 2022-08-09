19d7569be451a858211c92de79a77175595eeaeb
Dimimtar Dimmitrov <mitko.69@gmail.com>
2022-08-09 11:20:33 +0000

    initial

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "vm" {
+  ami           = "ami-0cabc39acf991f4f1"
+  subnet_id     = "subnet-0a464350c4a3c8a92"
+  instance_type = "t3.micro"
+  tags = {
+    Name = "my-tf-spawn-1"
+  }
+}
