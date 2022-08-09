7360cd5e8763f3daa02e012ff5e9ccb4664b668c
Dimitar Dimitrov <mitko.69@gmail.com>
2022-08-09 14:10:04 +0000

    added providers example

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+## Providers example
+provider "azurerm" {
+  features {}
+}
+
+## AWS
+provider "aws" {
+  region = "eu-central-1"
+}
+
 resource "aws_instance" "vm" {
   ami           = "ami-0cabc39acf991f4f1"
   subnet_id     = "subnet-0a464350c4a3c8a92"
