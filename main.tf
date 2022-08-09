c1a5ef9fca5158a9498eca9bec28b3d884545fa1
Dimitar Dimitrov <mitko.69@gmail.com>
2022-08-09 15:31:50 +0000

    Added Variables examples

--- main.tf
+++ main.tf
@@ -16,3 +16,39 @@ resource "aws_instance" "vm" {
     Name = "my-tf-spawn-1"
   }
 }
+
+## Variables
+# String
+variable "stringvar" {
+  type    = string
+  default = "Hi there!"
+}
+
+# List of strings
+variable "manystrings" {
+  type    = list(string)
+  default = [ "eu-cetral-1" ]
+}
+
+# List of objects
+variable "dockerPorts" {
+  type = list(object({
+    internal = number
+    external = number
+    protocol = string 
+  }))
+  default = [
+    {
+      internal = 8300
+      external = 8300
+      protocol = "tcp"
+    }
+  ]
+}
+
+## Output values
+# shown at terraform apply
+output "instance_ip" {
+  description = "VM private IP"
+  value = aws_instance.my-tf-spawn-1.private_ip
+}
